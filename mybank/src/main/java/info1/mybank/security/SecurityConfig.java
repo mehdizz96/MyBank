e4823f5132825a55fffb61f27ed99d465addcb02
mehdizz96 <98349205+mehdizz96@users.noreply.github.com>
2022-06-08 23:19:29 +0100

    tables users roles

--- mybank/src/main/java/info1/mybank/security/SecurityConfig.java
+++ mybank/src/main/java/info1/mybank/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package info1.mybank.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,16 +10,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN","USER");
-        auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
+        // auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN","USER");
+        // auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+        .usersByUsernameQuery("select username as principal, password as credentials,active from users where username=?")
+        .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
+        .rolePrefix("ROLE_")
+        .passwordEncoder(new MessageDigestPasswordEncoder("MD5"));
     }
     
     // @Bean
